5f0b8f9be6f4951efb9193077da730556cb5240e
EXA_GAMER_007 <salimovsarvar518@gmail.com>
2025-12-06 16:26:54 +0500

    suuuuuu

---
+++ funksiyamasala.py
@@ -0,0 +1,76 @@
+######1-MODUL######
+##1-masala
+# def hisobla(x):
+#     return f"Sonning kvadrati: {x*x}"
+# x = int(input("Sonni kiriting: "))
+#
+# print(hisobla(x))
+##2-masala
+# def takrorla(matn,son):
+#     return f"{matn*son}"
+# matn = input("Matnni kiriting: ").lower()
+# son = int(input("Takrorlash sonini kiring: "))
+# print(takrorla(matn,son))
+##3-masala
+# def aniqla(son):
+#     if son % 2 == 0:
+#         print('Juft!')
+#     else:
+#          print('Toq!')
+# son = int(input('Sonni kiriting: '))
+# print(aniqla(son))
+##4-masala
+# def hisobla(x,y):
+#     return f"Yig;indi {x+y}"
+# x = int(input("x sonni kiriting: "))
+# y = int(input("y sonni kiriting: "))
+# print(hisobla(x,y))
+##5-masala
+# def uzunlik(matn):
+#     hisob = 0
+#     for harf in matn:
+#         hisob += 1
+#     return f"Uzunlik: {hisob}"
+# matn = input("Matnni kiritin: ")
+# print(uzunlik(matn))
+##6-masala
+# def aniqla(son):
+#     if son < 0:
+#         print('Manfiy!')
+#     elif son > 0:
+#         print("Musbat!")
+#     elif son == 0:
+#         print("Nol!")
+#     else:
+#         print("XATOLIK")
+#         return f"XATOLIK!"
+# son = int(input("Sonni kiriting: "))
+# print(aniqla(son))
+##7-masala
+# def modul(son):
+#     if son < 0:
+#         print(-son)
+#     else:
+#         return son
+# son = int(input("Sonni kiriting: "))
+# print(modul(son))
+##8-masala
+# def teskari(matn):
+#     return f"Teskari qildim: {matn[::-1]}"
+# matn = input("Matinni kiriting: ")
+# print(teskari(matn))
+######2-MODUL######
+##1-masala
+# def faktorial(son):
+#     if son == 0 or son == 1:
+#         return 1
+#     return son * faktorial(son - 1)
+# print(faktorial(5))
+##2-masala
+
+
+
+
+
+
+
